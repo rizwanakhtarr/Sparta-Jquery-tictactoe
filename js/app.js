1511ece5ec5c63472bf5389609b44cddf0703ba9
rizwanakhtarr <you@example.com>
2018-06-01 03:26:39 +0100

    done changes to Jquery to make X and O plus the toggleclass function work on click

--- js/app.js
+++ js/app.js
@@ -1,4 +1,44 @@
 $(document).ready(function(){
-  // Code goes here
+  var $turn = $('#playerTurn');
+  // console.log(reset);
+
+  var anotherturn = 1;
+  var player1 = true;
+  // $reset.prepend("table");
+
+  $("td").click(function() {
+
+
+    if ($(this).text()=="" && player1) {
+      if ((anotherturn%2)==1) {
+         $(this).append("X");
+         $(this).toggleClass("X");
+        }
+      else { $(this).append("O");
+        }
+      anotherturn++;
+
+
+    }
+  });
+
+  
+
+  // $('#reset').click(function(){
+  //   $("td").remove();
+  // });
+
+
+
+  // $reset.children().hover();
+  // $("data-num").click(function() {
+  //   $(this).toggleClass('X');
+
+
+  // for (var i = 0; i < newreset.length; i++) {
+  //   $reset.append(`<table>${ reset[i] }</table>`); // using template lierals instead of concentation
+  // }
+  // // });
+
 
 });
